82bf788c00d8a74985d88b7e5f6a9fbf493e1a89
tecyang <me@tecyang.com>
2018-09-13 16:43:45 +0800

    添加 Spring Cloud Hystrix dashboard

--- SprintBoot/src/main/java/com/tecyang/sprint_boot_demo/controller/HelloController.java
+++ SprintBoot/src/main/java/com/tecyang/sprint_boot_demo/controller/HelloController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
-
     @RequestMapping("/Hello")
     public Object Index()
     {
--- ribbon-consumer/src/main/java/com/tecyang/ribbonconsumer/RibbonConsumerApplication.java
+++ ribbon-consumer/src/main/java/com/tecyang/ribbonconsumer/RibbonConsumerApplication.java
@@ -1,9 +1,10 @@
 package com.tecyang.ribbonconsumer;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 //开启服务发现
 @EnableDiscoveryClient
 //@SpringCloudApplication 默认包含上述注解 可仅使用该注解
+
 @SpringBootApplication
 public class RibbonConsumerApplication {
 
@@ -28,4 +30,21 @@ public class RibbonConsumerApplication {
     {
         return new RestTemplate();
     }
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);  //系统启动时加载顺序
+        registrationBean.addUrlMappings("/hystrix.stream");//路径
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+//    作者：running_house
+//    链接：https://www.jianshu.com/p/4cb1c48a9053
+//    來源：简书
+//    简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
+
 }
