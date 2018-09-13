1db6dd2c5dafa3a5df4267401b22bb1f87abde23
tecyang <me@tecyang.com>
2018-09-13 15:33:48 +0800

    添加 Spring Cloud Hystrix

--- ribbon-consumer/src/main/java/com/tecyang/ribbonconsumer/Controller/ConsumerController.java
+++ ribbon-consumer/src/main/java/com/tecyang/ribbonconsumer/Controller/ConsumerController.java
@@ -1,5 +1,6 @@
 package com.tecyang.ribbonconsumer.Controller;
 
+import com.tecyang.ribbonconsumer.Service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,13 +9,18 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
+//    @Autowired
+//    RestTemplate restTemplate;
+
     @Autowired
-    RestTemplate restTemplate;
+    HelloService helloService;
 
     @RequestMapping(value="/ribbon-consumer",method = RequestMethod.GET)
     public String helloConsumer()
     {
-        return restTemplate.getForEntity("http://HELLOSERVICE/Hello", String.class).getBody();
+//        return restTemplate.getForEntity("http://HELLOSERVICE/Hello", String.class).getBody();
+        return helloService.helloService();
     }
+
 
 }
--- ribbon-consumer/src/main/java/com/tecyang/ribbonconsumer/RibbonConsumerApplication.java
+++ ribbon-consumer/src/main/java/com/tecyang/ribbonconsumer/RibbonConsumerApplication.java
@@ -3,12 +3,18 @@ package com.tecyang.ribbonconsumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+//开启断路器功能
+@EnableCircuitBreaker
+//开启服务发现
 @EnableDiscoveryClient
+//@SpringCloudApplication 默认包含上述注解 可仅使用该注解
 @SpringBootApplication
 public class RibbonConsumerApplication {
 
