4574d4464dfc5427755ade84bfaf8063c75001a4
tecyang <me@tecyang.com>
2018-09-18 17:22:08 +0800

    Feign默认DEBUG日志功能开启及默认日志记录级别修改
    Signed-off-by: tecyang <me@tecyang.com>

--- feign-consumer/src/main/java/com/tecyang/feignconsumer/FeignConsumerApplication.java
+++ feign-consumer/src/main/java/com/tecyang/feignconsumer/FeignConsumerApplication.java
@@ -1,15 +1,22 @@
 package com.tecyang.feignconsumer;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringCloudApplication
 public class FeignConsumerApplication {
 
+    //默认feign 日志记录级别设置，也可以实现为配置类
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
     public static void main(String[] args) {
         SpringApplication.run(FeignConsumerApplication.class, args);
     }
